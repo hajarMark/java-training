ced0b5b4a40734dab0ca6bdb5290ddaf5f6e2fc7
Markaoui Hajar <markaouiha@cib.net>
2021-03-31 14:25:26 +0200

    PrimitiveTraining

---
+++ Tester.cmd
@@ -0,0 +1,4 @@
+javac -classpath out;lib/junit-4.12.jar -d out src/exercices/%1.java
+javac -classpath out;lib/junit-4.12.jar -d out src/tests/%1Test.java
+javac -classpath out;lib/junit-4.12.jar -d out src/tests/MainTest.java
+java -cp out;C:\Users\markaouiha\Wild\JavaProjects\Java-training\java-training\lib\junit-4.12.jar;C:\Users\markaouiha\Wild\JavaProjects\Java-training\java-training\lib\hamcrest-core-1.3.jar org.junit.runner.JUnitCore %1Test
